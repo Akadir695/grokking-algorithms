c362870a3872440c6d9403552f61e5609cc16c65
akadir <abdikadir.aweys@gmail.com>
2026-04-24 11:52:13 +0100

    add selection sort

---
+++ selection_sort/arrays_linked.py
@@ -0,0 +1,13 @@
+"""Arrays: elements are numbered, it starts from 0
+the position of arrays is called index
+Start at address 00
+Item     →  1st  2nd  3rd  4th  5th
+Index    →   0    1    2    3    4
+Lots of inserts → lean towards linked list
+Lots of reads → lean towards array
+You were close in your thinking, you just applied it the wrong way around! Does that make sense?
+the 4 is index of 4
+Fifth item is at address 04
+You can jump straight to it — no searching needed
+l
+"""
