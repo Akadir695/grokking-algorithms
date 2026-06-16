7e873cf5ab422c0b3afeaeeeb838b1d8c099a475
akadir <abdikadir.aweys@gmail.com>
2026-06-16 15:13:17 +0100

    Add hashtables folder

---
+++ hashtables/hash_tables.py
@@ -0,0 +1,16 @@
+# Hash tables is function where you put in string- meaning any data and you get back a number 
+"""it needs to constant meaning if you put apple you get 3 you should 3 all should get 3
+hash function maps different strings to diffrent indexes
+if we put hash function and array we get hash table , but in the realiy we use the same for diffrent items
+one -to-one mapping is called injective function
+python has hash tables which is called dictionaries
+to make hash table in python we use empty braces
+book = {}
+it has keys and values  book["apple" ]= 0.67 apples is keys and value is 0.67 
+"""
+book = {}
+book["apple" ]= 0.67
+book["milk" ]= 1.49
+book["avocado" ]= 1.49
+print(book) # {'apple': 0.67, 'milk': 1.49, 'avocado': 1.49}
+print(book["avocado"]) # 1.49
