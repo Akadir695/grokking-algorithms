f0825e080f3865403957716d810ca82d9dff875c
akadir <abdikadir.aweys@gmail.com>
2026-06-17 14:19:21 +0100

    finished the chapter

--- hashtables/hash_tables.py
+++ hashtables/hash_tables.py
@@ -14,3 +14,26 @@ book["milk" ]= 1.49
 book["avocado" ]= 1.49
 print(book) # {'apple': 0.67, 'milk': 1.49, 'avocado': 1.49}
 print(book["avocado"]) # 1.49
+# use cases
+"hash tables are great when you want to create one-to-one mapping and looking up things"
+phone_book = {}
+phone_book['jenny'] = '723894332'
+phone_book['ahmed'] = '732439222'
+print(phone_book["jenny"])
+# preventing duplicates we can filter duplicates
+voted = {}
+
+def check_voter(name):
+  if name in voted:
+    print(f'{name} has voted')
+  else:
+    voted[name] = True
+    print("let them vote")
+check_voter('akadir')
+check_voter('akadir')
+# we use hash tables as cache(it is common way to make things faster like like serving webpages to users)
+"collision happens when two keys are assigned to the same slot"
+"it does not matter if the hash table has one element or billion elements it always takes the same amount of time "
+"to avoid the the worst-case performance you need a load factor and good hash functio(one has has no collions)"
+
+
