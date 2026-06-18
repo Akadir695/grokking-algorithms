471bebc1913f03a31238d6916887747543138f40
akadir <abdikadir.aweys@gmail.com>
2026-06-18 18:46:51 +0100

    Add breadth-first search notes

---
+++ breadth-first-search/graph.py
@@ -0,0 +1,20 @@
+# Breadth-First Search (BFS) allows you to find the shortest distance between two things
+
+"""
+BFS solves the shortest-path problem — finding the shortest route between two nodes in a graph.
+
+Graphs:
+ A graph is a set of connections e.g. "alex owes money to rama" → alex -> rama
+ Graphs are made up of nodes and edges:
+ alex and rama are nodes
+the arrow (->) between them is the edge
+Graphs model how different things are connected to one another
+
+Queues (FIFO - First In, First Out):
+ Like a bus stop: if you arrive before your friend, you board the bus first
+In BFS, nodes are added to the back of the queue (enqueue) 
+and processed from the front (dequeue)
+the order is not a matter
+"""
+# implemention of algorithms python uses append and popleft instead of enqueue and dequeue
+
